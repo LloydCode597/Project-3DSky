39cbb5972b1e2ec1d94bb49d5bd3410c70dedbe2
Lloyd Allen <lloydallen597@gmail.com>
2026-08-20 23:35:53 -0500

    Add player movement and input handling
    Implement player movement and jumping in PlayerController: add serialized moveSpeed/jumpForce, input state fields, Rigidbody caching (Awake), FixedUpdate physics to apply horizontal velocity and jump via AddForce, and an IsGrounded raycast. Update input callbacks to store move and jump input values. Also update SampleScene transform (rotation/position) and set PlayerController defaults (moveSpeed=7, jumpForce=14).

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,14 +3,55 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    private Vector2 moveInput;
+    private bool jumpInput;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Physics code will go here.
+        Vector3 velocity = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed;
+        rig.linearVelocity = velocity;
+
+        if (jumpInput && IsGrounded())
+        {
+            jumpInput = false;
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+
+        jumpInput = false;
+        velocity.y = rig.linearVelocity.y;
+
+        rig.linearVelocity = velocity;
+
+    }
+
+    bool IsGrounded()
+    {
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.1f, 0), Vector3.down, 0.2f))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-        Vector2 inputVector = context.ReadValue<Vector2>();
-        Debug.Log(inputVector);
+        moveInput = context.ReadValue<Vector2>();
     }
     public void OnJumpInput(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValueAsButton());
+        jumpInput = context.ReadValueAsButton();
     }
 
 }
